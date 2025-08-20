fa8bac751c0fb4ff189f1d1baa58bd3622613c4b
suvadeep-ship-it <starrydreamer23@gmail.com>
2025-08-20 21:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("initial commit")
 console.log("second commit on main from open source project")
 console.log("third commit on main from open source project")
+console.log("some work done on the open source project") 
